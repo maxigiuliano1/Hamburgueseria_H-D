064c78d7c2538e4d1fa65cb59648e0bd1f0f9d45
maxi giuliano <maxigiuliano18@gmail.com>
2022-05-15 22:42:28 -0300

    cambios estesticos en la seccion de testimonios

--- ReactJs_App/src/components/inicio/TestimoniosItem.js
+++ ReactJs_App/src/components/inicio/TestimoniosItem.js
@@ -3,26 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/Main.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {Avatar} from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
 
 const TestimoniosItem = (props) => {
     const { message, image } = props
 
     return (
         <div>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-                <div style={{width: '50%', display: 'flex', alignItems: 'center', flexDirection: 'row', textAlign: 'center'}}>
-                        <Avatar 
-                            imgProps={{style: {borderRadius: '50%'}}}
-                            src={image} 
-                            style={{
-                                width: 150, 
-                                height: 150, 
-                                border: '1px solid lightgray', 
-                                padding: 7
-                            }}
-                        />
-                        <p style={{marginLeft: 25}}>{message}</p>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div style={{ width: '50%', display: 'flex', alignItems: 'center', flexDirection: 'row', textAlign: 'center' }}>
+                    <Avatar
+                        imgProps={{ style: { borderRadius: '50%' } }}
+                        src={image}
+                        style={{
+                            width: 150,
+                            height: 150,
+                            border: '1px solid lightgray',
+                            padding: 7
+                        }}
+                    />
+                    <p style={{ marginLeft: 25 }}>{message}</p>
                 </div>
             </div>
         </div>
